8a6220265940540b2a95cf9d3996c0921b945311
Hoyoun Song <hysong0129@gmail.com>
2019-08-11 02:14:24 +0900

    Initial Commit

---
+++ Hephaestus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hephaestus
+//
+//  Created by Hoyoun Song on 11/08/2019.
+//  Copyright © 2019 Douglas Edan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
